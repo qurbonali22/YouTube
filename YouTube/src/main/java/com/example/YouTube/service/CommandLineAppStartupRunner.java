8dcbe81ebbc04b48ac506a69a884388600754f92
qurbonali22 <allidele574@gmail.com>
2022-12-22 12:58:25 +0500

    log configuration

--- YouTube/src/main/java/com/example/YouTube/service/CommandLineAppStartupRunner.java
+++ YouTube/src/main/java/com/example/YouTube/service/CommandLineAppStartupRunner.java
@@ -1,5 +1,6 @@
 package com.example.YouTube.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,14 +9,17 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 
 @Component
+@Slf4j
 public class CommandLineAppStartupRunner implements CommandLineRunner {
-
 
     @Autowired
     private DataSource dataSource;
 
     @Override
     public void run(String... args) throws Exception {
+        log.info("CommandLineAppStartupRunner ishini boshladi");
         Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
     }
+
+
 }
