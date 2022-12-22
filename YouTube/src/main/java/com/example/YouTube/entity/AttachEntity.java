d8d8649783b2cc89fa56c4ea54a25c143e4f9e6c
qurbonali22 <allidele574@gmail.com>
2022-12-22 13:33:20 +0500

    Create attach entity

---
+++ YouTube/src/main/java/com/example/YouTube/entity/AttachEntity.java
@@ -0,0 +1,40 @@
+package com.example.YouTube.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "attach")
+public class AttachEntity {
+
+    @Id
+    @GenericGenerator(name = "attach_uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @Column(name = "original_name")
+    private String originalName;
+
+    @Column
+    private String path;
+
+    @Column
+    private Long size;
+
+    @Column
+    private String extension;
+
+    @Column
+    private Long duration;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+}
