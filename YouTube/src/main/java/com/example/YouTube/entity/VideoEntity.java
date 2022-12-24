e24af3a6e271af1d5a008c4bff5826fb56bedf55
qurbonali22 <allidele574@gmail.com>
2022-12-24 21:18:03 +0500

    video entity fixed

--- YouTube/src/main/java/com/example/YouTube/entity/VideoEntity.java
+++ YouTube/src/main/java/com/example/YouTube/entity/VideoEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "video")
 public class VideoEntity {
     @Id
+    @GeneratedValue(generator = "generator_uuid")
     @GenericGenerator(name = "video_uuid", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
     @Column
@@ -43,6 +44,7 @@ public class VideoEntity {
     @Column(name = "published_date")
     private LocalDateTime publishedDate;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "video_type")
     private VideoType type;
 
